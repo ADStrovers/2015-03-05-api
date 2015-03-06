09432d67366f43550c58dbb94a5b759ba08b949f
Andrew Strovers <ADStrovers@gmail.com>
2015-03-06 12:14:16 -0600

    Working state.  Could definitely be cleaned up

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ window.onload = function() {
   
   button[0].addEventListener("click", returnAllStudents);
   
-  var form = document.getElementById("fileinfo");
+  var form = document.getElementById("student_id");
   form.addEventListener('submit', returnSingleStudent);
   
   function returnAllStudents() {
@@ -22,11 +22,18 @@ window.onload = function() {
     js_req.send();
   }
   
-  function returnSingleStudent() {
-    alert("first");
-    var formdata = new FormData(document.getElementsById("fileinfo"));
-    console.log(formdata);
-    alert("second");
+  function returnSingleStudent(e) {
+    e.preventDefault();
+    var js_req = new XMLHttpRequest;
+    var formValue = document.getElementsByTagName("input")[0].value;
+    js_req.addEventListener("load", function(e) {
+      var response = JSON.parse(js_req.response);
+      var string = "Name: " + response.name + ", Age: " + response.age
+      var a = document.getElementsByClassName("ubae");
+      a[0].innerHTML = string;
+    });
+    js_req.open("get", "http://localhost:4567/students/" + formValue)
+    js_req.send();
   }
 
 }
